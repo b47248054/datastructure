6236a1bc46127db1bc65fcab867c8fc7358501ce
liuzhongxu <softotto@126.com>
2020-04-21 16:50:44 +0800

    Leetcode 1248

--- src/test/java/linear/TestArray.java
+++ src/test/java/linear/TestArray.java
@@ -199,4 +199,51 @@ public class TestArray {
         }
         return new int[]{};
     }
+
+    /**
+     * 1248. 统计「优美子数组」
+     * 给你一个整数数组 nums 和一个整数 k。
+     *
+     * 如果某个 连续 子数组中恰好有 k 个奇数数字，我们就认为这个子数组是「优美子数组」。
+     *
+     * 请返回这个数组中「优美子数组」的数目。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [1,1,2,1,1], k = 3
+     * 输出：2
+     * 解释：包含 3 个奇数的子数组是 [1,1,2,1] 和 [1,2,1,1] 。
+     * 示例 2：
+     *
+     * 输入：nums = [2,4,6], k = 1
+     * 输出：0
+     * 解释：数列中不包含任何奇数，所以不存在优美子数组。
+     * 示例 3：
+     *
+     * 输入：nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+     * 输出：16
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/count-number-of-nice-subarrays
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int numberOfSubarrays(int[] nums, int k) {
+        int sum = 0, res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i] % 2; // &1
+            if (map.containsKey(sum - k)) {
+                res += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+
+    }
 }
