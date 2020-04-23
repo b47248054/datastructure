8932ac0add3549a7ac5d0dfe9e4384dc9db60c59
liuzhongxu <softotto@126.com>
2020-04-23 18:12:14 +0800

    leetcode 62 dp

---
+++ src/test/java/linear/TestDP.java
@@ -0,0 +1,61 @@
+package linear;
+
+import org.junit.Test;
+
+/**
+ * @author liuzhongxu
+ * @date 2020/4/23
+ */
+public class TestDP {
+
+    /**
+     * 62. 不同路径
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     *
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+     *
+     * 问总共有多少条不同的路径？
+     *
+     *
+     *
+     * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/unique-paths
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *     作者：帅地
+     *     链接：https://www.zhihu.com/question/23995189/answer/1094101149
+     *     来源：知乎
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[m][n]; //
+        // 初始化
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++){
+            dp[0][i] = 1;
+        }
+        // 推导出 dp[m-1][n-1]
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    @Test
+    public void testUniquePaths() {
+        int ans = uniquePaths(99, 99);// c76*c21*c21
+        System.out.println(ans);
+    }
+}
